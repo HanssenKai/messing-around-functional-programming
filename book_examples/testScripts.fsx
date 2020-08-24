e765d68914aae3ba85ed70e595259aa49295ed53
HanssenKai <kha244@post.uit.no>
2020-08-24 14:49:44 +0200

    added more string contatenations

--- book_examples/testScripts.fsx
+++ book_examples/testScripts.fsx
@@ -12,6 +12,8 @@ let twoToFive = [2;3;4;5]        // Square brackets create a list with
                                  // semicolon delimiters.
 let oneToFive = 1 :: twoToFive   // :: creates list with new 1st element
 // The result is [1;2;3;4;5]
+let oneToSix = oneToFive @ [7]
+printfn "testing print %A" oneToSix
 let zeroToFive = [0;1] @ twoToFive   // @ concats two lists
 
 // IMPORTANT: commas are never used as delimiters, only semicolons!
