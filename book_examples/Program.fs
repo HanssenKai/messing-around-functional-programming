770ff030bf849b6bb16f946d68395f9c6a2762bb
HanssenKai <kha244@post.uit.no>
2020-08-25 15:43:44 +0200

    added basic module usage

--- book_examples/Program.fs
+++ book_examples/Program.fs
@@ -1,8 +1,15 @@
 ﻿// Learn more about F# at http://fsharp.org
 
+
 open System
+open TypeExamples 
+open somefunc
+
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let y = add1 20
+    printfn "num is %d" y
     0 // return an integer exit code
+
