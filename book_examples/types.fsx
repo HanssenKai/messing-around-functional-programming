911e8511822689d3af29830e0afe439f5372d222
HanssenKai <kha244@post.uit.no>
2020-08-25 12:51:17 +0200

    testing f# function types

---
+++ book_examples/types.fsx
@@ -0,0 +1,31 @@
+//basic function
+let add5 x = x + 5
+
+let eval5Add2  x = add5 x  + 2
+
+let times3 x = x * 3
+
+// function input
+let eval5Add2Func fn x = fn x + 2
+
+let y = eval5Add2Func times3 8
+
+
+
+// function output
+let adderGenerator numberToAdd = (+) numberToAdd
+
+let add1 = adderGenerator 1
+let add2 = adderGenerator 2 
+
+let x = 2 + add1 2
+
+let eval5AsInt (fn:int->int) x = fn x + 5
+
+let j = eval5AsInt add2 4 
+// j = 4 + 2 + 5
+
+
+
+
+
