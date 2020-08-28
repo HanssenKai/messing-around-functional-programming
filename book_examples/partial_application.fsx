4c2d316769587a5d573cc8041972e9ed3320f90e
HanssenKai <kha244@post.uit.no>
2020-08-28 10:16:39 +0200

    partial application examples

--- book_examples/partial_application.fsx
+++ book_examples/partial_application.fsx
@@ -7,12 +7,10 @@ printer 5
 
 //list manipulation
 let add1 = (+) 1
-let add1ToEach = List.map add1 
+let add1ToEach = List.map add1
 
-add1ToEach [1;2;3;4;5;5]
+add1ToEach [ 1; 2; 3; 4; 5; 5 ]
 
-let filterEvens = 
-    List.filter (fun i -> i%2 = 0)
+let filterEvens = List.filter (fun i -> i % 2 = 0)
 
-filterEvens[1;2;3;4;5;6]
-
+filterEvens [ 1; 2; 3; 4; 5; 6 ]
